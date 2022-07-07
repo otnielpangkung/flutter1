d2c473ae9c9f1f3d6b05ee847af1165c6bc3735b
otnielpangkung <otnielpangkung@yahoo.co.id>
2022-07-07 23:17:51 +0800

    7Jul

--- lib/pages/bonus.dart
+++ lib/pages/bonus.dart
@@ -10,50 +10,100 @@ class BonusPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Column(children: [
-          SizedBox(height: 120,),
-          Container(
-            width: 300,
-            height: 200,
-            padding: EdgeInsets.all(24),
-            decoration:  BoxDecoration(image: DecorationImage(image: AssetImage("assets/card-gradiant.png"))),
-         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
+        child: Column(
           children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              Expanded(
-                child: Column(
+            SizedBox(
+              height: 120,
+            ),
+            Container(
+              width: 300,
+              height: 200,
+              padding: EdgeInsets.all(24),
+              decoration: BoxDecoration(
+                  image: DecorationImage(
+                      image: AssetImage("assets/card-gradiant.png"))),
+              child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Text("Name", style:whiteTextStyle.copyWith(fontWeight: light, fontSize: 14) ,),
-                    Text("Kezie Anne Salalaaam Mamama", style:whiteTextStyle.copyWith(fontWeight: medium, fontSize: 20), overflow: TextOverflow.ellipsis,)
-                  ],
-                ),
-              ),
-              Container(width: 24, margin: EdgeInsets.only(right: 6),height: 24, decoration: BoxDecoration(image: DecorationImage(image:AssetImage("assets/airplane-icon.png"))),),
-              Text("PAY",  style:whiteTextStyle.copyWith(fontWeight: light, fontSize: 14),)
-            ],
-          ),
-          SizedBox(height: 41,),
-          Text("Balance", style: whiteTextStyle.copyWith(fontWeight: light, fontSize: 14) ,),
-          Text("IDR 280.000.000", style: whiteTextStyle.copyWith(fontWeight: semiBold, fontSize: 26) ,),
-
-         ]),
-         
-          ),
-         SizedBox(height: 91,),
-          Container(
-            width: 200,
-            height: 48,
-            decoration:  BoxDecoration(image: DecorationImage(image: AssetImage("assets/big-bonus.png"))),
-          ),
-         SizedBox(height: 10,),
-         Text("We give you early credit so that \n you can buy a flight ticket", 
-         style: greyTextStyle.copyWith(fontWeight: light, fontSize: 16),textAlign: TextAlign.center,),
-         CostumButton(title: "Start Fly Now", margin: EdgeInsets.only(top: 50), width: 220, onPress: () {}),
-        ],),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Expanded(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                "Name",
+                                style: whiteTextStyle.copyWith(
+                                    fontWeight: light, fontSize: 14),
+                              ),
+                              Text(
+                                "Kezie Anne Salalaaam Mamama",
+                                style: whiteTextStyle.copyWith(
+                                    fontWeight: medium, fontSize: 20),
+                                overflow: TextOverflow.ellipsis,
+                              )
+                            ],
+                          ),
+                        ),
+                        Container(
+                          width: 24,
+                          margin: EdgeInsets.only(right: 6),
+                          height: 24,
+                          decoration: BoxDecoration(
+                              image: DecorationImage(
+                                  image:
+                                      AssetImage("assets/airplane-icon.png"))),
+                        ),
+                        Text(
+                          "PAY",
+                          style: whiteTextStyle.copyWith(
+                              fontWeight: light, fontSize: 14),
+                        )
+                      ],
+                    ),
+                    SizedBox(
+                      height: 41,
+                    ),
+                    Text(
+                      "Balance",
+                      style: whiteTextStyle.copyWith(
+                          fontWeight: light, fontSize: 14),
+                    ),
+                    Text(
+                      "IDR 280.000.000",
+                      style: whiteTextStyle.copyWith(
+                          fontWeight: semiBold, fontSize: 26),
+                    ),
+                  ]),
+            ),
+            SizedBox(
+              height: 91,
+            ),
+            Container(
+              width: 200,
+              height: 48,
+              decoration: BoxDecoration(
+                  image: DecorationImage(
+                      image: AssetImage("assets/big-bonus.png"))),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Text(
+              "We give you early credit so that \n you can buy a flight ticket",
+              style: greyTextStyle.copyWith(fontWeight: light, fontSize: 16),
+              textAlign: TextAlign.center,
+            ),
+            CostumButton(
+                title: "Start Fly Now",
+                margin: EdgeInsets.only(top: 50),
+                width: 220,
+                onPress: () {
+                  Navigator.pushNamed(context, "/home");
+                }),
+          ],
+        ),
       ),
     );
   }
--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -13,6 +13,159 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Container(
+        margin: EdgeInsets.symmetric(horizontal: 24),
+        child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+          Container(
+            margin: EdgeInsets.only(top: 30),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text("Howdy\nKezia Anne",
+                    style: blackTextStyle.copyWith(
+                      fontWeight: bold,
+                      fontSize: 24,
+                    )),
+                Container(
+                  width: 50,
+                  height: 50,
+                  decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      image: DecorationImage(
+                          image: AssetImage("assets/profile.png"))),
+                )
+              ],
+            ),
+          ),
+          SizedBox(
+            height: 6,
+          ),
+          Text(
+            "Where to fly today?",
+            style: greyTextStyle.copyWith(fontWeight: light, fontSize: 16),
+            textAlign: TextAlign.left,
+          ),
+          SizedBox(
+            height: 30,
+          ),
+          SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            child: Row(
+              children: [
+                desinationBox("Lake Ciliung", 4.8, "Tangerang", "assets/lake-ciliung.png"),
+                desinationBox("White House", 4.7, "Spain", "assets/white-house.png"),
+                desinationBox("hili Heyo", 4.8, "Monaco", "assets/hill-heyo.png"),
+                desinationBox("Menara", 5.0, "Japan", "assets/menarra.png"),
+              ],
+            ),
+          ),
+          SizedBox(
+            height: 50,
+          ),
+          Text("New This Year", style: blackTextStyle.copyWith(fontWeight: bold, fontSize: 18),),
+          SizedBox(
+            height: 34,
+          ),
+          Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Container(
+                    width: 70,
+                    height: 70,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(defaultRadius),
+                      image: DecorationImage(image: AssetImage("assets/tumb/australia.png"))),
+                  ),
+                  
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text("Danau Berantan", style: blackTextStyle.copyWith(fontWeight: semiBold, fontSize: 18),),
+                      Text("Singapura",style: greyTextStyle.copyWith(fontWeight: light, fontSize: 14)),
+                    ],
+                  ),
+                   Container(
+                      width: 24,
+                      height: 24,
+                      decoration: BoxDecoration(
+                          image: DecorationImage(
+                              image: AssetImage("assets/star.png"))),
+                    ),
+                    Text("4.8")
+                ],
+                
+              )
+            ],
+          )
+        ]),
+      ),
+    );
+  }
+
+  Container desinationBox(
+    String title, double rating, String country, String imageUrl
+   ) {
+    // final String placeDestenation;
+    // final String country;
+    // final double score;
+    return Container(
+      margin: EdgeInsets.only(right: 19),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            child: Align(
+              alignment: Alignment.topRight,
+              child: Container(
+                width: 55,
+                height: 30,
+                decoration: BoxDecoration(
+                    color: whiteColor,
+                    borderRadius: BorderRadius.only(
+                        bottomLeft: Radius.circular(defaultRadius))),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Container(
+                      width: 24,
+                      height: 24,
+                      decoration: BoxDecoration(
+                          image: DecorationImage(
+                              image: AssetImage("assets/star.png"))),
+                    ),
+                    Text( 
+                      rating.toString(),
+                      style: blackTextStyle.copyWith(
+                          fontWeight: semiBold, fontSize: 14),
+                    )
+                  ],
+                ),
+              ),
+            ),
+            width: 180,
+            height: 220,
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(defaultRadius),
+                image: DecorationImage(
+                    image: AssetImage(imageUrl))),
+          ),
+          Container(
+              margin: EdgeInsets.only(top: 8),
+              child: Text(
+                title,
+                style:
+                    blackTextStyle.copyWith(fontSize: 18, fontWeight: semiBold),
+              )),
+          SizedBox(
+            height: 3,
+          ),
+          Text(country,
+              style: blackTextStyle.copyWith(fontSize: 14, fontWeight: light)),
+        ],
+      ),
+    );
   }
 }
